e3dad0368bf558933028b93954d44658f22590b9
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-01-22 15:17:17 +0100

    Ändrade namn från alumniberattelser -> alumniblogg

--- pages/alumniblogg/[alumniberattelse].js
+++ pages/alumniblogg/[alumniberattelse].js
@@ -2,14 +2,15 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import MarkdownRender from '../../components/MarkdownRender';
 function Alumniberattelse() {
+    // För att kunna läsa länkadressen
     const router = useRouter();
-    const { alumniberattelse } = router.query;
+    const { alumniberattelse } = router.query; // Länken är densamma som filnamnet
     return (
         <div id="contentbody">
             <h1>Alumniblogg</h1>
             <article>
                 <MarkdownRender
-                    source={`../content/alumniberattelser/${alumniberattelse}.md`}
+                    source={`../content/alumniblogg/${alumniberattelse}.md`}
                 />
             </article>
         </div>
