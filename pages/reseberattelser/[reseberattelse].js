c46cc259fe7e1cf0fe9558aad7049cfa3f24fa93
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-02-04 00:02:33 +0100

    Pre-renders för alumni och rese sidorna

--- pages/reseberattelser/[reseberattelse].js
+++ pages/reseberattelser/[reseberattelse].js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import MarkdownRender from '../../components/MarkdownRender';
-function Reseberattelse() {
-    const router = useRouter();
-    const { reseberattelse } = router.query;
+import { join } from 'path';
+import { readdirSync } from 'fs';
+function Reseberattelse({ reseberattelse }) {
     return (
         <div id="contentbody">
             <h1>Reseberättelse</h1>
             <article className="rese">
                 <MarkdownRender
-                    source={`../content/reseberattelser/${reseberattelse}.md`}
+                    source={`../content/reseberattelser/${reseberattelse.reseberattelse}.md`}
                 />
             </article>
         </div>
     );
 }
 export default Reseberattelse;
+
+export async function getStaticProps(context) {
+    const { params } = context;
+    return {
+        props: { reseberattelse: params }, // will be passed to the page component as props
+    };
+}
+
+export async function getStaticPaths() {
+    const PATH = join(process.cwd(), 'public/content/reseberattelser');
+    const paths = readdirSync(PATH)
+        .map((path) => path.replace(/\.mdx?$/, ''))
+        .map((blogid) => ({ params: { reseberattelse: blogid } }));
+
+    return {
+        paths,
+        fallback: false, // can also be true or 'blocking'
+    };
+}
