623be1655374e9130799da14e082d3a22beb6183
Armin Baymani <webbutvecklare@cl-sektionen.se>
2023-02-19 02:07:45 +0100

    fixade tabs istället för knapp till kalendersidan

--- pages/kalender.js
+++ pages/kalender.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 
 function Kalender(){
-    const [show, setShow] = useState(true);
-    function toggleKalender(knapp){
-        setShow(!show);
-        knapp.innerHTML = show? "Visa Sektionskalendern <i class=\"fa-regular fa-calendar\"/>" : "Visa Gråttkalendern  <i class=\"fa-regular fa-calendar\"/>";
+    const [activeIdx, setActiveIdx] = useState(0)
+    function setCalendar(index){
+        setActiveIdx(index);
     }
 
     return (
@@ -16,20 +15,22 @@ function Kalender(){
                 Om du är intresserad av att boka sektionslokalen kan du enkelt göra detta genom att maila lokalansvarig på:
                 <br/><strong>lokalnamnden@cl-sektionen.se</strong>
             </p>
-            <div className="controlls_tab">
-                <p>
-                    <b>Psst!!</b>  Du kan exportera sektionens kalendrar för att komma åt dem enklare.<br/>
-                    <a href="https://calendar.google.com/calendar/embed?src=c_5sqhb0om2kmti770g06qqknfik%40group.calendar.google.com&ctz=Europe%2FBerlin">
-                        Tryck här <i className="fa-solid fa-arrow-up-right-from-square"/></a>
-                    &nbsp;för att exportera <strong>sektionskalendern</strong>.<br/>
-                    <a href="https://calendar.google.com/calendar/u/0/embed?height=700&wkst=2&bgcolor=%23616161&ctz=Europe/Stockholm&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=1&showCalendars=0&showTz=0&mode=MONTH&src=MDA4Z3BsM3M3ODd0ZTZqaGlwazU3MjljNmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=a29uc3VtY2x3QGdtYWlsLmNvbQ&color=%237CB342&color=%23AD1457&pli=1">
-                        Tryck här <i className="fa-solid fa-arrow-up-right-from-square"/></a>
-                    &nbsp;för att exportera <strong>gråttkalendern</strong>.
-                </p>
-                <button className="kal_knapp" onClick={(e) => {toggleKalender(e.target);}}>Visa Gråttkalendern <i className="fa-regular fa-calendar"/></button>
+            <p>
+                <b>Psst!!</b>  Du kan exportera sektionens kalendrar för att komma åt dem enklare.<br/>
+                <a href="https://calendar.google.com/calendar/embed?src=c_5sqhb0om2kmti770g06qqknfik%40group.calendar.google.com&ctz=Europe%2FBerlin">
+                    Tryck här <i className="fa-solid fa-arrow-up-right-from-square"/></a>
+                &nbsp;för att exportera <strong>sektionskalendern</strong>.<br/>
+                <a href="https://calendar.google.com/calendar/u/0/embed?height=700&wkst=2&bgcolor=%23616161&ctz=Europe/Stockholm&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=1&showCalendars=0&showTz=0&mode=MONTH&src=MDA4Z3BsM3M3ODd0ZTZqaGlwazU3MjljNmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=a29uc3VtY2x3QGdtYWlsLmNvbQ&color=%237CB342&color=%23AD1457&pli=1">
+                    Tryck här <i className="fa-solid fa-arrow-up-right-from-square"/></a>
+                &nbsp;för att exportera <strong>gråttkalendern</strong>.
+            </p>
+
+            <div className="tabs-wrapper">
+                <button onClick={() => {setCalendar(0)}} className={`kal-knapp sektionskalender ${(activeIdx === 0)? "active" : ""}`}>Sektionskalender</button>
+                <button onClick={() => {setCalendar(1)}} className={`kal-knapp gråttkalender ${(activeIdx === 1)? "active" : ""}`}>Gråttans kalender</button>
             </div>
             
-            <div id="sektionskal" className="kalender_tab" style={{display: show? "block":"none"}}>
+            <div id="sektionskal" className="kalender_tab" style={{display: activeIdx === 0? "block":"none"}}>
                 <h1 className="kal_titel">Sektionskalendern</h1>
                 <iframe id="open-web-calendar" 
                     style={{background: "url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat"}}
@@ -38,7 +39,7 @@ function Kalender(){
                     frameBorder="0" height="750px" width="100%">
                 </iframe>
             </div>
-            <div id="gråttkal" className="kalender_tab" style={{display: show? "none":"block"}}>
+            <div id="gråttkal" className="kalender_tab" style={{display: activeIdx === 1? "block":"none"}}>
                 <h1 className="kal_titel">Gråttans kalender</h1>
                 <iframe id="open-web-calendar" 
                     style={{background: "url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat"}}
