a55a6cf6487559bb42efeb58e126ce79e84a5752
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-03-13 13:47:46 +0100

    Format change

--- components/FeedPreview.js
+++ components/FeedPreview.js
@@ -1,62 +1,53 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import parse from 'html-react-parser';
-import sanitizeHtml from 'sanitize-html';
+import Link from "next/link";
+import Image from "next/image";
+import parse from "html-react-parser";
+import sanitizeHtml from "sanitize-html";
 
-import bg from '../public/media/img/KTHcover.jpg';
+import bg from "../public/media/img/KTHcover.jpg";
 
 const FeedPreview = ({ docs, title }) => {
-    let post = {};
-    return (
-        <div className="feed-preview">
-            {docs.map(
-                (doc) => (
-                    (doc.data().id = doc.id),
-                    ((post = doc.data()),
-                    (
-                        <Link href={`/aktuellt/${doc.id}`} key={doc.id}>
-                            <div className="post-preview">
-                                <div className="image">
-                                    {post.image && <img src={post.image} />}
-                                    {!post.image && (
-                                        <Image
-                                            src={bg}
-                                            alt="Bakgrundsbild KTH"
-                                        />
-                                    )}
-                                </div>
-                                <div className="post-meta">
-                                    <h2>{post.title}</h2>
-                                    {/*här fanns post.author förut, kan läggas tillbaka om plats finns*/}
-                                    <div>
-                                        <p>
-                                            {post.publishDate
-                                                .toDate()
-                                                .toLocaleDateString('sv')}
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="post-content">
-                                    <p className="subtitle">{post.subtitle}</p>
-                                        {/* Parse för att formatera om html koden till faktiska element
+  let post = {};
+  return (
+    <div className="feed-preview">
+      {docs.map(
+        (doc) => (
+          (doc.data().id = doc.id),
+          ((post = doc.data()),
+          (
+            <Link href={`/aktuellt/${doc.id}`} key={doc.id}>
+              <div className="post-preview">
+                <div className="image">
+                  {post.image && <img src={post.image} />}
+                  {!post.image && <Image src={bg} alt="Bakgrundsbild KTH" />}
+                </div>
+                <div className="post-meta">
+                  <h2>{post.title}</h2>
+                  {/*här fanns post.author förut, kan läggas tillbaka om plats finns*/}
+                  <div>
+                    <p>{post.publishDate.toDate().toLocaleDateString("sv")}</p>
+                  </div>
+                </div>
+                <div className="post-content">
+                  <p className="subtitle">{post.subtitle}</p>
+                  {/* Parse för att formatera om html koden till faktiska element
                                         Sanitize för att göra det lite mer stilrent i previewn dvs inga styles*/}
-                                    <div className="body">
-                                        <p>
-                                            {parse(
-                                                sanitizeHtml(post.body, {
-                                                    allowedTags: [],
-                                                })
-                                            )}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-                    ))
-                )
-            )}
-        </div>
-    );
+                  <div className="body">
+                    <p>
+                      {parse(
+                        sanitizeHtml(post.body, {
+                          allowedTags: [],
+                        })
+                      )}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))
+        )
+      )}
+    </div>
+  );
 };
 
 export default FeedPreview;
