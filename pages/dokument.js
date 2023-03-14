645a763aa7fff42af1b35bc37e185896b230ce35
Armin Baymani <webbutvecklare@cl-sektionen.se>
2023-03-14 17:08:32 +0100

    lade till CLs SM-ord

--- pages/dokument.js
+++ pages/dokument.js
@@ -11,9 +11,10 @@ function Dokument(){
                 <h2>Blanketter och mallar</h2>
                 <a href="https://drive.google.com/file/d/1rOzE5IwIRqV0D89qd5f0i-CnTebz8Y3h/view"><i className="fa-regular fa-file-pdf"/> Utläggsblankett</a>
                 <a href="https://drive.google.com/file/d/1aBCjU8wfLI5NwNNPjf-RabC1G4ZxM-wn/view"><i className="fa-regular fa-file-pdf"/> Milersättningsblankett</a>
-                <a href="https://docs.google.com/document/d/1R9VzViVigsH3GzNqoHZJUYT3qf6Nw878BST8yCr3Dik/edit"><i className="fa-regular fa-file-pdf"/> Mall för entledigande</a> 
-                <a href="https://docs.google.com/document/d/17srFoYElH16ysq_xeu_jlue3W4cxYeJsYgRA2FRWLus/edit"><i className="fa-regular fa-file-pdf"/> Mall för motion</a> 
-                <a href="https://docs.google.com/document/d/1-LNWlpYvrYVFwJ9H9fZZO4-a9XAB8KLULQGBnuCX8pQ/edit"><i className="fa-regular fa-file-pdf"/> Mall för äskan</a> 
+                <a href="https://docs.google.com/document/d/1R9VzViVigsH3GzNqoHZJUYT3qf6Nw878BST8yCr3Dik"><i className="fa-regular fa-file-pdf"/> Mall för entledigande</a> 
+                <a href="https://docs.google.com/document/d/17srFoYElH16ysq_xeu_jlue3W4cxYeJsYgRA2FRWLus"><i className="fa-regular fa-file-pdf"/> Mall för motion</a> 
+                <a href="https://docs.google.com/document/d/1-LNWlpYvrYVFwJ9H9fZZO4-a9XAB8KLULQGBnuCX8pQ"><i className="fa-regular fa-file-pdf"/> Mall för äskan</a> 
+                <a href="https://docs.google.com/document/d/1iKTCjjld17XxJUKBo9cWCmr1A_vmsATD"><i className="fa-regular fa-file-pdf"/> CL:s SM-ord</a> 
             </div>
             <h2>Styrdokument</h2>
             <iframe src="https://drive.google.com/embeddedfolderview?id=1Nwg-S7C0YZ0FAeoQtQoQs2NpAcB9Jacb#grid" style={{width: "100%", height: "600px", border: "0", backgroundColor: "#F2F3F4"}}></iframe><br/>
