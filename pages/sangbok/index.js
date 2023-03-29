0553e63ee9f9e48ac0e8ec7fbfd12780e20ed694
Webbutvecklare-CL <121131893+Webbutvecklare-CL@users.noreply.github.com>
2023-03-29 12:46:12 +0200

    Update index.js

--- pages/sangbok/index.js
+++ pages/sangbok/index.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 import { useState } from "react";
 
 const sånger = [
-    { title: "Porthos visa", href: "/porthos_visa" },
-    { title: "Bordeaux Bordeaux", href: "/bordeaux_bordeaux" },
-    { title: "Vikingen", href: "/vikingen" }
+    { "title": "Porthos visa", "href": "/porthos_visa" },
+    { "title": "Bordeaux Bordeaux", "href": "/bordeaux_bordeaux" },
+    { "title": "Vikingen", "href": "/vikingen" }
 ]
 
 function Sangbok() {
@@ -19,13 +19,19 @@ function Sangbok() {
                 type="text" 
                 placeholder="Sök efter sång..."
                 onChange={(e) => setSearch(e.target.value)}
+                className="searchbar"
             ></input>
             <br></br>
 
             {sånger.filter((sång) => {
                 return search.toLowerCase() === "" ? sång : sång.title.toLowerCase().includes(search)
             }).map((sång) =>
-                <Link href={`sangbok${sång.href}`}>{sång.title} <br></br></Link>
+                <Link 
+                    href={`sangbok${sång.href}`}
+                    className="sånglänk"
+                >
+                    {sång.title}<br></br>
+                </Link>
             )}
         </div>
     );
