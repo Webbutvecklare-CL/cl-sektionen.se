1c3d451e3bd12c9859647a6cfad50ba95aa61a19
Webbutvecklare-CL <121131893+Webbutvecklare-CL@users.noreply.github.com>
2023-03-29 03:14:15 +0200

    bytte namn på sånger för att det blev oläsligt

---
+++ pages/sangbok/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function Sangbok() {
+    return (
+        <div id="contentbody">
+            <h1>Sångbok</h1>
+            <p>Här kommer hända saker snart!</p>
+        </div>
+    );
+}
+
+export default Sangbok;
