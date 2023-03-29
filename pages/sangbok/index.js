e9b7d1d374dd1e44435fb1fb3d89861d8c79381e
Webbutvecklare-CL <121131893+Webbutvecklare-CL@users.noreply.github.com>
2023-03-29 12:39:17 +0200

    implementerade sökning

--- pages/sangbok/index.js
+++ pages/sangbok/index.js
@@ -1,14 +1,32 @@
 import Link from "next/link";
+import { useState } from "react";
+
+const sånger = [
+    { title: "Porthos visa", href: "/porthos_visa" },
+    { title: "Bordeaux Bordeaux", href: "/bordeaux_bordeaux" },
+    { title: "Vikingen", href: "/vikingen" }
+]
 
 function Sangbok() {
+    const [search, setSearch] = useState("")
+    console.log(search)
     return (
         <div id="contentbody">
             <h1>Sångbok</h1>
             <p>Här kommer hända saker snart!</p>
 
-            {/*(test)*/}
-            <Link href="/sangbok/porthos_visa">Porthos visa</Link><br>
-            <Link href="/sangbok/bordeaux_bordeaux">bordeaux bordeaux</Link>
+            <input 
+                type="text" 
+                placeholder="Sök efter sång..."
+                onChange={(e) => setSearch(e.target.value)}
+            ></input>
+            <br></br>
+
+            {sånger.filter((sång) => {
+                return search.toLowerCase() === "" ? sång : sång.title.toLowerCase().includes(search)
+            }).map((sång) =>
+                <Link href={`sangbok${sång.href}`}>{sång.title} <br></br></Link>
+            )}
         </div>
     );
 }
