95f7eb332606219e5f89e57f5caa5a3503efcb6c
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-04-03 00:03:42 +0200

    Cloud functions - med revalidate

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,28 @@
 import functions, { logger } from "firebase-functions";
+import fetch from "node-fetch";
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-export const updateUser = functions.firestore
-  .region("europe-west3")
-  .document("posts/{postId}")
-  .onUpdate((change, context) => {
+export const createPost = functions
+  .region("europe-west1")
+  .firestore.document("posts/{postId}")
+  .onCreate(() => {
+    logger.log("Post skapad");
+
     // Revalidate:ar hemsidan
-    fetch(`https://${process.env.DOMAIN}/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}`)
-      .then((res) => {
-        if (res.ok) {
-          res.json().then((data) => {
-            logger.log(data);
-          });
-        }
-      })
-      .catch(logger.warn("Could not revalidate pages"));
+    revalidate();
+
+    // Skickar notis till alla som följer eventuella taggar
+
+    return 0;
+  });
+
+export const updatePost = functions
+  .region("europe-west1")
+  .firestore.document("posts/{postId}")
+  .onUpdate((change, context) => {
+    logger.log("Post uppdaterad");
+
+    // Revalidate:ar hemsidan
+    revalidate();
 
     const newValue = change.after.data();
     const previousValue = change.before.data();
@@ -27,6 +33,42 @@ export const updateUser = functions.firestore
       // Kollar om det är en ny bild i så fall: Ta bort den förra bilden
     }
 
-    logger.log("Post uppdaterad");
+    // Om tid ändras skicka notis om det
+
+    return 0;
   });
+
+export const deletePost = functions
+  .region("europe-west1")
+  .firestore.document("posts/{postId}")
+  .onDelete(() => {
+    logger.log("Post borttagen");
+
+    // Revalidate:ar hemsidan
+    revalidate();
+
+    // Ta bort eventuell kopplad bild
+
+    // Rensa alla notis prenumerationer
+
+    return 0;
+  });
+
+function revalidate() {
+  const url = `${process.env.DOMAIN}/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}`;
+  const headers = {
+    "Content-Type": "application/json",
+    "method": "GET",
+  };
+  logger.log("Sending revalidation request");
+  fetch(url, headers)
+    .then((res) => {
+      if (res.ok) {
+        logger.log("Revalidation successful");
+      } else {
+        logger.warn("Revalidation unsuccessful", res.statusText);
+      }
+    })
+    .catch((error) => logger.error(error));
+}
 
