9053fb8de74070a6349b580d05a99145722fc2d0
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-04-05 19:32:21 +0200

    Notiser för nya event och nyheter

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,33 @@
+import admin from "firebase-admin";
 import functions, { logger } from "firebase-functions";
 import fetch from "node-fetch";
-
+admin.initializeApp();
 export const createPost = functions
   .region("europe-west1")
   .firestore.document("posts/{postId}")
-  .onCreate((snapshot, context) => {
-    logger.log("Post skapad");
-
-    logger.log(context.params);
+  .onCreate(async (snapshot, context) => {
+    logger.log("Inlägg skapat ", context.params.postId);
     // Revalidate:ar hemsidan
     revalidate("all");
     revalidate("post", context.params.postId);
 
     // Skickar notis till alla som följer eventuella taggar
+    const payload = createPayload(snapshot);
+    // Hämtar alla tokens - de som ska få notisen
+    const allTokens = new Set();
+    const fcmTokensCollection = admin.firestore().collection("fcmTokens");
     const type = snapshot.data().type;
-    const payload = {
-      notification: {
-        title: `${snapshot.data().committee} publicerade ${
-          type == "Event" ? "ett event" : "en nyhet"
-        }`,
-        body: `${snapshot.data().title}`,
-        // icon: snapshot.data().image || "/images/profile_placeholder.png",
-        click_action: `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com/aktuellt/${snapshot.id}`,
-      },
-    };
+    if (type === "Event") {
+      const eventTokenDoc = await fcmTokensCollection.doc("event").get();
+      const eventTokens = eventTokenDoc.data().tokens;
+      eventTokens.forEach((token) => allTokens.add(token));
+    } else if (type === "Nyheter") {
+      const newsTokenDoc = await fcmTokensCollection.doc("event").get();
+      const newsTokens = newsTokenDoc.data().tokens;
+      newsTokens.forEach((token) => allTokens.add(token));
+    }
+
+    sendNotification(payload, Array.from(allTokens));
 
     return 0;
   });
@@ -70,12 +74,12 @@ export const deletePost = functions
   });
 
 function revalidate(page = "all", postId = "") {
-  const url = `${process.env.DOMAIN}/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}&page=${page}&postId=${postId}`;
+  const url = `https://${process.env.DOMAIN}/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}&page=${page}&postId=${postId}`;
   const headers = {
     "Content-Type": "application/json",
     "method": "GET",
   };
-  logger.log("Sending revalidation request");
+  logger.log("Sending revalidation request for " + page);
   fetch(url, headers)
     .then((res) => {
       if (res.ok) {
@@ -87,12 +91,14 @@ function revalidate(page = "all", postId = "") {
     .catch((error) => logger.error(error));
 }
 
+// Sends the notification to all tokens
 async function sendNotification(payload, tokens) {
+  logger.log(`Sending notification to ${tokens.length} devices`);
   if (tokens.length > 0) {
     // Send notifications to all tokens.
     const response = await admin.messaging().sendToDevice(tokens, payload);
     await cleanupTokens(response, tokens);
-    functions.logger.log("Notifications have been sent and tokens cleaned up.");
+    logger.log("Notifications have been sent and tokens cleaned up.");
   }
 }
 
@@ -103,7 +109,7 @@ function cleanupTokens(response, tokens) {
   response.results.forEach((result, index) => {
     const error = result.error;
     if (error) {
-      functions.logger.error("Failure sending notification to", tokens[index], error);
+      logger.error("Failure sending notification to", tokens[index], error);
       // Cleanup the tokens that are not registered anymore.
       if (
         error.code === "messaging/invalid-registration-token" ||
@@ -116,4 +122,18 @@ function cleanupTokens(response, tokens) {
     }
   });
   return Promise.all(tokensDelete);
+}
+
+// Create payload
+function createPayload(snapshot) {
+  const data = snapshot.data();
+  const type = data.type;
+  return {
+    notification: {
+      title: `${data.committee} publicerade ${type == "Event" ? "ett event" : "en nyhet"}`,
+      body: `${data.title}`,
+      // icon: snapshot.data().image || "/images/profile_placeholder.png",
+      click_action: `https://${process.env.DOMAIN}/aktuellt/${snapshot.id}`,
+    },
+  };
 }
--- pages/firebasetest.js
+++ pages/firebasetest.js
@@ -1,14 +1,37 @@
-import React, { useMemo } from "react";
-
-import { firestore } from "../firebase/clientApp";
-import enableMessaging from "../firebase/messaging";
+import React, { useState } from "react";
+import { saveMessagingDeviceToken } from "../firebase/messaging";
 
 export default function Firebase() {
+  const [result, setResult] = useState("");
+
+  const handleSubscribe = async (topic) => {
+    const res = await saveMessagingDeviceToken(topic);
+    if (res.message) {
+      setResult("Något gick fel");
+    } else {
+      setResult("Du prenumererar nu på " + topic);
+    }
+  };
+
   return (
     <div id="contentbody">
       <h1>Firebase</h1>
       <div>
-        <button onClick={enableMessaging}>Notiser</button>
+        <button
+          onClick={() => {
+            handleSubscribe("event");
+          }}>
+          Notiser för event
+        </button>
+        <button
+          onClick={() => {
+            handleSubscribe("news");
+          }}>
+          Notiser för news
+        </button>
+      </div>
+      <div>
+        <p>{result}</p>
       </div>
     </div>
   );
