b769789ea0e5e74d16dead17f94557945c524727
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-04-15 23:22:18 +0200

    Fixade problem med att skapa konton

--- components/personalrummet/Login.js
+++ components/personalrummet/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { googleLogin, validateAccountCheck, createUser } from "../../utils/authUtils";
+import { googleLogin } from "../../utils/authUtils";
 import { useAuth } from "../../context/AuthContext";
 
 import { GoogleAuthProvider } from "firebase/auth";
@@ -11,79 +11,59 @@ export default function Login() {
   const [screen, setScreen] = useState("login");
   const [error, setError] = useState("");
 
-  const { user, setUserData, logOut, setUserAccessToken } = useAuth();
+  const { user, setUserAccessToken, signUp, getUserData } = useAuth();
 
-  const handleLogin = () => {
-    googleLogin()
-      .then((result) => {
-        console.log("Manuellt inloggad!");
-        // Användaren har loggat in med sin förtroendevalds-mail
-        // Validera kontot om det finns i databasen och har permission samt nämndtillhörighet
-        validateAccountCheck(result.user)
-          .then((data) => {
-            if (data) {
-              data.uid = result.user.uid;
-              // Kontot finns inlagt i systemet
-              console.log("Inloggad");
-              setUserData(data);
+  const handleLogin = async () => {
+    // Om det inte finns ett konto inloggat få användaren logga in med sitt google konto
+    if (!user) {
+      console.log("Manuellt inloggad!");
+      try {
+        const res = await googleLogin();
 
-              // Sparar accessToken för Google API (typ kalender) lokalt
-              const credential = GoogleAuthProvider.credentialFromResult(result);
-              const token = credential.accessToken;
-              setUserAccessToken(token);
-            } else {
-              // Kontot är inte inlagt i systemet
-              console.log(
-                "Din mailadress är inte inlagd i systemet eller så saknar du behörighet!"
-              );
-              setScreen("signup");
-            }
-          })
-          .catch((err) => {
-            // Om något gått snett vid valideringen eller inladdningen av posts
-            // ska användaren loggas ut
-            console.error(err);
-            setError("Något gick fel vid valideringen!");
-            if (user) {
-              logOut();
-            }
-          });
-      })
-      .catch((err) => {
+        // Provar att spara accessToken
+        const credential = GoogleAuthProvider.credentialFromResult(res);
+        if (credential) {
+          console.log("Token sparad");
+          const token = credential.accessToken;
+          setUserAccessToken(token);
+        }
+      } catch (err) {
+        // Fel med manuella inloggningen
         console.error(err);
         if (err.code == "auth/popup-closed-by-user") {
           setError("Inloggningsfönstret stängdes!");
         } else {
           setError("Fel vid inloggningen till google!");
         }
-      });
+      }
+    } else {
+      // Det finns en användare inloggad men den har inte blivit registrerad i databasen eller fått sin behörighet
+      console.log("Hämtar user data");
+      try {
+        const res = await getUserData(user);
+        console.log(res);
+        if (res === "no permission") {
+          setScreen("no permission");
+        } else if (res === "no uid") {
+          setScreen("signup");
+        }
+      } catch (err) {
+        setError(err.toString());
+      }
+    }
   };
 
   const handleSignup = () => {
-    // Om den förtroendevalda inte finns i user databasen kan den lägga in sin mail och sitt namn
-    googleLogin()
-      .then((result) => {
-        // Användaren har loggat in
-        try {
-          // Försöker skapa ett doc i user databasen
-          createUser(result.user);
-          setScreen("login");
-        } catch (err) {
-          // Om något gått fel med skapandet av användar-doc så loggas användaren ut
-          console.error("Fel vid inloggningen: ", err);
-          if (user) {
-            logOut();
-          }
-          setError("Det gick inte att lägga till kontot. Kolla i konsolen efter felmeddelande.");
-          return;
-        }
+    signUp()
+      .then(() => {
+        console.log("Uppgifter sparade");
+        setScreen("login");
       })
       .catch((err) => {
-        console.error(err);
-        if (error.code == "auth/popup-closed-by-user") {
-          setError("Inloggningsfönstret stängdes!");
+        console.error("Fel vid inloggningen: ", err.err);
+        if (err.message) {
         } else {
-          setError("Fel vid inloggningen till google!");
+          setError("Det gick inte att lägga till kontot. Kolla i konsolen efter felmeddelande.");
         }
       });
   };
@@ -106,16 +86,28 @@ export default function Login() {
           <button onClick={handleLogin}>Logga in</button>
         </div>
       )}
-      {screen == "signup" && (
+      {screen === "signup" && (
         <div>
+          <h3>Ditt konto verka inte vara inlagt i personalsystemet...</h3>
           <p>
-            Ditt konto verka inte vara inlagt i personalsystemet...
+            (1) Klicka på &#34;Lägg till konto&#34; och logga in igen för att lägga till din mail i
+            systemet.
             <br />
-            Klicka på &#34;Lägg till konto&#34; och logga in igen för att lägga till din mail i
-            systemet. Meddela därefter någon av de webbansvariga att du vill ha behörighet till
+            (2) Meddela därefter någon av de webbansvariga att du vill ha behörighet till
             personalrummet.
           </p>
           <button onClick={handleSignup}>Lägg till konto</button>
+        </div>
+      )}
+      {screen === "no permission" && (
+        <div>
+          <h3>Du är registrerad men du saknar behörighet till personalrummet.</h3>
+          <p>Be eller invänta att webbansvariga uppdatera din behörighet.</p>
+          <p>
+            När du har fått bekräftat att du är inlagd och fått rätt behörighet kan du testa att
+            logga in igen.
+          </p>
+          <button onClick={handleLogin}>Logga in</button>
         </div>
       )}
     </div>
