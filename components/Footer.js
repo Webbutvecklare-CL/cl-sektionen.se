258680e1d3d16c095d7582d65c15e77934ae96e3
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-04-16 20:42:47 +0200

    La till snabblänkar i footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,41 +8,51 @@ export default function Navbar() {
     <footer>
       <div id="footerinfo">
         <Image src={CL_logo_stor} alt="CL logo, stor" className="footer_logo" />
-        <div id="footerinfotext">
-          <h3>Organisationsnummer</h3>802420-8491
-          <br />
-          <h3>Bankgiro</h3>
-          Huvudsektionen: 251-5310
-          <br />
-          ClubWästeriet: 5160-8172
-          <br />
-          Mottagningen: 5160-8206
-          <h3>Faktureringsadress</h3>
-          Sektionen för Civilingenjör och Lärare
-          <br />
-          Drottning Kristinas väg 15
-          <br />
-          100 44 Stockholm
-          <h3>Leveransadress</h3>
-          Osquars backe 8
-          <br />
-          114 28 Stockholm
-        </div>
-        <div id="sociallinks">
-          <a style={{ color: "inherit" }} href="https://www.instagram.com/clsektionen/">
-            <i className="fa-brands fa-instagram"></i>
-          </a>
-          <a style={{ color: "inherit" }} href="https://www.facebook.com/groups/2388999847/">
-            <i className="fa-brands fa-facebook"></i>
-          </a>
-          <a
-            style={{ color: "inherit" }}
-            href="https://github.com/Webbutvecklare-CL/cl-sektionen.se/">
-            <i className="fa-brands fa-github"></i>
-          </a>
-          <Link href="/personalrummet" style={{ color: "inherit" }}>
-            <i className="fa-solid fa-mug-hot"></i>
-          </Link>
+        <div className="footer-row">
+          <div id="footerinfotext" className="footer-col">
+            <h3>Organisationsnummer</h3>802420-8491
+            <br />
+            <h3>Bankgiro</h3>
+            Huvudsektionen: 251-5310
+            <br />
+            ClubWästeriet: 5160-8172
+            <br />
+            Mottagningen: 5160-8206
+            <h3>Faktureringsadress</h3>
+            Sektionen för Civilingenjör och Lärare
+            <br />
+            Drottning Kristinas väg 15
+            <br />
+            100 44 Stockholm
+            <h3>Leveransadress</h3>
+            Osquars backe 8
+            <br />
+            114 28 Stockholm
+          </div>
+          <div id="quick-links" className="footer-col">
+            <h3>Hitta snabbt</h3>
+            <Link href={"/for-foretag"}>För företag</Link>
+            <Link href={"/aktuellt"}>Aktuellt</Link>
+            <Link href={"/kontakt"}>Kontakt</Link>
+            <Link href={"/hjalp-vid-illabehandling"}>Illabehandling</Link>
+            <Link href={"/dokument"}>Dokument</Link>
+          </div>
+          <div id="sociallinks" className="footer-col">
+            <a style={{ color: "inherit" }} href="https://www.instagram.com/clsektionen/">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+            <a style={{ color: "inherit" }} href="https://www.facebook.com/groups/2388999847/">
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+            <a
+              style={{ color: "inherit" }}
+              href="https://github.com/Webbutvecklare-CL/cl-sektionen.se/">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <Link href="/personalrummet" style={{ color: "inherit" }}>
+              <i className="fa-solid fa-mug-hot"></i>
+            </Link>
+          </div>
         </div>
       </div>
       <iframe
--- public/content/kontakt/naringsliv.md
+++ public/content/kontakt/naringsliv.md
@@ -1,7 +1,13 @@
-## Näringsliv
+## Näringsliv och socialamedier
 
-Sektionen samarbetar ofta och gärna med näringslivet. Du hittar kontakt och mer info på [För företag](/for-foretag).
+Sektionen samarbetar ofta och gärna med näringslivet. Du hittar kontakt och mer info på [För företag](/for-foretag).  
+Om du vill ha något upplagt på socialamedier eller på vår webbplats kontakta våran PR-ansvarig.
 
 ### Näringslivsnämndens ordförande
 
 [ordf.naringsliv@cl-sektionen.se](mailto:ordf.naringsliv@cl-sektionen.se)
+
+### PR-ansvarig
+
+Tuva Egman  
+[pransvarig@cl-sektionen.se](pransvarig@cl-sektionen.se)
