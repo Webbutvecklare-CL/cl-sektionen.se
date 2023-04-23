33e83f2f43eec8625d81a2d6d8f95c5319dc769b
Webbutvecklare-CL <121131893+Webbutvecklare-CL@users.noreply.github.com>
2023-04-23 12:34:11 +0200

    cleanup

---
+++ components/GråttKalender.js
@@ -0,0 +1,19 @@
+const GråttKalender = () =>{
+    return (
+    <div className="mini_kalender kalender_tab gråttan månad">
+        <h1 className="kal_titel">Gråttbokningar</h1>
+        <iframe
+          id="open-web-calendar månad"
+          className="open-web-calendar"
+          style={{
+            background:
+              "url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat",
+          }}
+          src="https://kalendern-cl.vercel.app/calendar.html?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fkonsumclw%2540gmail.com%2Fpublic%2Fbasic.ics&amp;url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2F008gpl3s787te6jhipk5729c6g%2540group.calendar.google.com%2Fpublic%2Fbasic.ics&amp;language=sv&amp;tab=month"
+          sandbox="allow-scripts allow-same-origin allow-top-navigation"
+          height="400px"
+          width="100%">
+        </iframe>
+      </div>
+    )
+}; export default GråttKalender;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Sidhuvud_inv from "../public/media/grafik/sidhuvud_inv.png";
 import React, { useState } from "react";
 import FeedPreview from "../components/FeedPreview";
 import GråttAgenda from "../components/GråttAgenda";
+import GråttKalender from "../components/GråttKalender";
 import FeaturedPostPreview from "../components/FeaturedPostPreview";
 
 // För text rendering
@@ -89,21 +90,7 @@ export default function Index({ contents, featured, infoList, eventList }) {
             width="100%"></iframe>
           <GråttAgenda className="agenda-vy" />
           <br />
-          <div className="mini_kalender kalender_tab gråttan månad">
-            <h1 className="kal_titel">Gråttbokningar</h1>
-            <iframe
-              id="open-web-calendar månad"
-              className="open-web-calendar"
-              style={{
-                background:
-                  "url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat",
-              }}
-              src="https://kalendern-cl.vercel.app/calendar.html?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fkonsumclw%2540gmail.com%2Fpublic%2Fbasic.ics&amp;url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2F008gpl3s787te6jhipk5729c6g%2540group.calendar.google.com%2Fpublic%2Fbasic.ics&amp;language=sv&amp;tab=month"
-              sandbox="allow-scripts allow-same-origin allow-top-navigation"
-              height="400px"
-              width="100%">
-            </iframe>
-          </div>
+          <GråttKalender/>
         </section>
         <hr />
         <section className="resurser">
