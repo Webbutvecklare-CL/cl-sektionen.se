a77a587831bec01d72bf0b2e205a8c6de40be8c6
Webbutvecklare-CL <121131893+Webbutvecklare-CL@users.noreply.github.com>
2023-05-07 20:44:50 +0200

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+export default function Custom404() {
+    return (
+        <div id="contentbody">
+            <div className="error-msg">
+                <span className="error-code">404</span>
+                <div className="devider">|</div>
+                <span className="error-description">Sidan kunde inte hittas</span>
+            </div>
+        </div>
+    );
+}
