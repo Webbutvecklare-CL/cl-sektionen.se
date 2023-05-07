617c4b79d5c1644836a225b43a07162fbaf132c4
Webbutvecklare-CL <121131893+Webbutvecklare-CL@users.noreply.github.com>
2023-05-07 18:33:36 +0200

    skapade en template för ny sida

---
+++ templates/pageTemplate.js
@@ -0,0 +1,21 @@
+import React from "react";
+import MarkdownRender from "../components/MarkdownRender";
+
+//Create <pagename>.js file in "pages" directory. Url will become cl-sektionen.se/<pagename>
+export default function Page({ contents }) {
+  return (
+    <div id="contentbody">
+      <h1 id="page-title">{/* Title of page goes here */}</h1>
+      {/* Page contents go here*/}
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const contents = getContentData(/* Content name */);
+  return {
+    props: {
+      contents,
+    }, // will be passed to the page component as props
+  };
+}
