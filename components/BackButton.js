0d6ced2b6ef291c7d9c6b829dc3175ae3d5d4c6b
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-05-09 19:20:14 +0200

    Fixade tillbaka knapp Tidigare när en användare klickat in på länken från en annan sida kom den till bka till den sidan.

--- components/BackButton.js
+++ components/BackButton.js
@@ -3,8 +3,19 @@ import React from "react";
 
 export default function BackButton() {
   const router = useRouter();
+
+  function handleBackClick() {
+    // Kollar om den förra sidan var från webbplatsen eller extern
+    const previousUrl = document.referrer;
+    if (previousUrl || !previousUrl.startsWith(window.location.origin)) {
+      router.push("/");
+    } else {
+      router.back();
+    }
+  }
+
   return (
-    <button onClick={() => router.back()}>
+    <button className="back-button" onClick={handleBackClick}>
       <i className="fa fa-arrow-left" aria-hidden="true"></i> Tillbaka
     </button>
   );
