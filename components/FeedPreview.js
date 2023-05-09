2bb24ec8f8be4e14feada8443ac6db92d012ce49
Webbutvecklare-CL <121131893+Webbutvecklare-CL@users.noreply.github.com>
2023-05-10 00:21:08 +0200

    ...

--- components/FeedPreview.js
+++ components/FeedPreview.js
@@ -29,7 +29,7 @@ export default function FeedPreview({ posts }) {
                 </div>
                 <div className="post-meta">
                   <h2>{post.title}</h2>
-                  {post.author} {date.toLocaleDateString()}
+                  {post.author} {date.getFullYear()}
                 </div>
                 <div className="post-content">
                   <p className="subtitle">{post.subtitle}</p>
