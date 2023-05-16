d4cf36a6fc00c0443b6761dbe6c1a2f06d2e2a34
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-05-16 15:25:02 +0200

    Clean up

--- utils/convertDate.js
+++ utils/convertDate.js
@@ -1,18 +1,18 @@
 //Används för att konvertera datumet som är sparad i firebase till datumsträng på yyyy-mm-dd format
 //Eftersom .toLocaleString() gav error i production build.
-export function convertDate(date){
-    var y = String(date.getUTCFullYear());
-    var m = String(date.getUTCMonth() + 1);
-    var d = String(date.getUTCDate());
-    
-    if (m.length < 2) {
+export function convertDate(date) {
+  var y = String(date.getUTCFullYear());
+  var m = String(date.getUTCMonth() + 1);
+  var d = String(date.getUTCDate());
+
+  if (m.length < 2) {
     m = "0" + m;
-    }
+  }
 
-    if (d.length < 2) {
+  if (d.length < 2) {
     d = "0" + d;
-    }
+  }
 
-    var res = [y, m, d].join("-");
-    return res.includes("NaN") ? "" : res;
+  var res = [y, m, d].join("-");
+  return res.includes("NaN") ? "" : res;
 }
