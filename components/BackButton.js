d60cffcaea93f4ccbda6dbb4f6f6a77dc4b31837
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-05-27 01:24:04 +0200

    Lösning på Sångbokens tillbakaknapp #117 Bättre lösning där den inte leder en tillbaka till ex facebook vore bra

--- components/BackButton.js
+++ components/BackButton.js
@@ -1,22 +1,27 @@
 import { useRouter } from "next/router";
+import Router from "next/router";
 import React from "react";
 
 export default function BackButton() {
   const router = useRouter();
 
   function handleBackClick() {
-    // Kollar om den förra sidan var från webbplatsen eller extern
-    const previousUrl = document.referrer;
-    if (previousUrl || !previousUrl.startsWith(window.location.origin)) {
+    // Användaren har en historik som går att gå tillbaka i.
+    // Optimalt vore att kolla om historiken är från vår sida och inte typ FB.
+    // Om historiken är 1 så är det den aktuella sidan dvs ingen historik
+    if (window?.history?.length < 2) {
+      // Om användaren öppnat en länk till sång/inlägg i en ny flik
       router.push("/");
-    } else {
-      router.back();
+      return;
     }
+
+    // Kan leda en till baka till facebook eller till ex sångboken
+    router.back();
   }
 
   return (
     <button className="back-button" onClick={handleBackClick}>
-      <i className="fa fa-arrow-left" aria-hidden="true"></i> Tillbaka
+      <i className="fa fa-arrow-left" aria-hidden="true" /> Tillbaka
     </button>
   );
 }
