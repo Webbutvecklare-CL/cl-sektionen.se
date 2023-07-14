ac6d508c75d721e2ac1a1aefc8bebed4d2c1fc27
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-07-14 05:13:38 +0200

    La till lite debug grejer

--- pages/firebasetest.js
+++ pages/firebasetest.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { saveMessagingDeviceToken } from "../firebase/messaging";
+import { saveMessagingDeviceToken } from "../firebase/messaging"; // Filen
+import { isSupported } from "firebase/messaging"; // Biblioteket
 import { sendNotification } from "../utils/server";
 
 import { useAuth } from "../context/AuthContext";
 
 export default function Firebase() {
   const [result, setResult] = useState("");
+  const [debugText, setDebugText] = useState("");
   const [notificationMessage, setNotificationMessage] = useState("");
 
   const { userData } = useAuth();
@@ -17,8 +19,10 @@ export default function Firebase() {
         "Det gick inte att prenumerera på notiser.\n\nDu nekade tillåtelse eller så stödjs inte notiser på din enhet eller webbläsare."
       );
       setResult("Något gick fel");
+      setDebugText(res.message);
     } else {
       setResult("Du prenumererar nu på " + topic);
+      setDebugText("Token: " + res);
     }
   };
 
@@ -26,40 +30,62 @@ export default function Firebase() {
     sendNotification(userData?.uid, notificationMessage);
   };
 
+  const testSupport = () => {
+    isSupported().then((yes) => {
+      setDebugText(yes ? "Notiser stödjs" : "Notiser stödjs inte");
+    });
+  };
+
   return (
     <div id="contentbody">
-      <h1>Firebase</h1>
-      <div>
-        <button
-          onClick={() => {
-            handleSubscribe("event");
-          }}>
-          Notiser för event
-        </button>
-        <button
-          onClick={() => {
-            handleSubscribe("information");
-          }}>
-          Notiser för news
-        </button>
-      </div>
-      <div>
-        <p>{result}</p>
-      </div>
+      <article>
+        <h1>Notis testar centrum</h1>
+        <p>
+          Här testar vi notiser. Om du vill vara med och testa kan du välja vilken kategori du vill
+          få notiser ifrån, du kan välja båda. Om du är osäker på om din enhet stödjer notiser tryck
+          på &quot;Kolla support&quot;.
+        </p>
+        <div>
+          <button
+            onClick={() => {
+              handleSubscribe("event");
+            }}>
+            Notiser för event
+          </button>
+          <button
+            onClick={() => {
+              handleSubscribe("information");
+            }}
+            style={{ marginLeft: "20px" }}>
+            Notiser för information
+          </button>
 
-      <div>
-        <h3>Skicka notis</h3>
-        <input
-          type="textarea"
-          name=""
-          id=""
-          value={notificationMessage}
-          onChange={(e) => {
-            setNotificationMessage(e.target.value);
-          }}
-        />
-        <button onClick={handleSendNotification}>Skicka notis</button>
-      </div>
+          <button style={{ marginLeft: "20px" }} onClick={testSupport}>
+            Kolla support
+          </button>
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          <p>{result}</p>
+          <p>Debug: {debugText}</p>
+        </div>
+
+        {/* Skicka notis kan användas av inloggade */}
+        {userData && (
+          <div>
+            <h3>Skicka notis</h3>
+            <textarea
+              type="textarea"
+              rows="1"
+              cols="30"
+              value={notificationMessage}
+              onChange={(e) => {
+                setNotificationMessage(e.target.value);
+              }}
+            />
+            <button onClick={handleSendNotification}>Skicka notis</button>
+          </div>
+        )}
+      </article>
     </div>
   );
 }
