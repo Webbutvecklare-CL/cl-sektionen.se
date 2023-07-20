98d422da9e99ce95fbcaceb756494885589ff851
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-07-20 14:46:49 +0200

    Test för att se vad som fixar sketen

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  runtimeCaching,
+  //   runtimeCaching,
 });
 
 const nextConfig = {
