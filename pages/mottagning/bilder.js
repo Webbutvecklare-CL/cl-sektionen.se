3d8a817d5c410c9d888709a77f7ec3e7333c70ff
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-07-20 19:38:21 +0200

    Testar lite på mottagnings bilder

--- pages/mottagning/bilder.js
+++ pages/mottagning/bilder.js
@@ -6,6 +6,22 @@ export default function Bilder() {
       <BackButton page="mottagning">Mottagningssidan</BackButton>
       <div>
         <p>Här hittar du en/flera länkar till alla bilder</p>
+        <h2>Första veckan</h2>
+        <ul>
+          <li>Måndag</li>
+          <li>Tisdag</li>
+          <li>Onsdag</li>
+          <li>Torsdag</li>
+          <li>Fredag</li>
+        </ul>
+        <h2>Andra veckan</h2>
+        <ul>
+          <li>Måndag</li>
+          <li>Tisdag</li>
+          <li>Onsdag</li>
+          <li>Torsdag</li>
+          <li>Fredag</li>
+        </ul>
       </div>
     </div>
   );
