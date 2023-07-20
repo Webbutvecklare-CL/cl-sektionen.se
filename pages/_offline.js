7742032c774d5c09348cdfb4e4c3e2a489de4e27
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-07-20 19:11:52 +0200

    La till en offline sida och fixade lite styes

--- pages/_offline.js
+++ pages/_offline.js
@@ -9,10 +9,12 @@ export default function Custom404() {
     <div id="contentbody" className={styles.body}>
       <Image src={bg} alt="background logo" className={styles.background}></Image>
       <div className={styles.errorMsg}>
-        <span className={styles.errorCode}>404</span>
+        <span className={styles.errorCode}>Ingen anslutning</span>
         <span className={styles.divider} />
-        <span className={styles.errorDescription}>Sidan kunde inte hittas</span>
-        <p></p>
+        <span className={styles.errorDescription}>
+          Sidan du försöker besöka går inte att nå i offline-läge. <br />
+          Kontrollera din internet anslutning.
+        </span>
       </div>
       <div className={styles.errorBackLink}>
         <BackButton>Startsidan</BackButton>
