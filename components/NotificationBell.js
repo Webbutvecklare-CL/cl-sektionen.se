1648fa5fd909acc5326b35f0a9acf0b789a86fbf
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-07-26 22:38:38 +0200

    Fixade klocka med hint

--- components/NotificationBell.js
+++ components/NotificationBell.js
@@ -1,22 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "../styles/notification-bell.module.css";
 import NotificationModal from "./NotificationModal";
+import { isSupported } from "firebase/messaging";
 
 export default function NotificationBell({}) {
+  const [showBell, setShowBell] = useState(true);
   const [showNotificationSettings, setShowNotificationSettings] = useState(false);
+
+  const [showHint, setShowHint] = useState(false);
+
+  useEffect(() => {
+    // Hinten visas inte förutom om notiser är supportat och användaren inte tidigare tillåtit notiser
+    isSupported().then((supported) => {
+      if (!supported) {
+        return;
+      } else {
+        // Kollar om användaren tidigare har tillåtit notiser och om inställningar finns
+        // Om något saknas visas en hinten
+        if (
+          Notification.permission !== "granted" &&
+          !localStorage.getItem("notificationSettings")
+        ) {
+          setTimeout(() => {
+            setShowHint(true);
+          }, 1000);
+          setTimeout(() => {
+            setShowHint(false);
+          }, 6000);
+        }
+      }
+    });
+  }, []);
 
   // Kolla support innan den visar modal
 
+  if (!showBell) return <></>;
+
   return (
     <>
-      <span
+      <div
         className={styles.bell}
         onClick={() => {
+          setShowHint(false);
           setShowNotificationSettings(true);
         }}>
-        <i className="fa-solid fa-bell" />
-      </span>
+        <span className={`${styles.hint} ${!showHint ? styles.hidden : ""}`}>
+          <span>Slå på notiser</span>
+        </span>
+        <span>
+          <i className="fa-solid fa-bell" />
+        </span>
+      </div>
       {showNotificationSettings && (
         <NotificationModal
           show={showNotificationSettings}
