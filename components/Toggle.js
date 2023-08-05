9b0c09a7b2a51760c5d53eab2c91952999e0fe42
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-08-05 22:01:27 +0200

    Fixade en och dokumenterade en bugg

--- components/Toggle.js
+++ components/Toggle.js
@@ -1,7 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { toggle as styles } from "../styles/components.module.css";
 export default function Toggle({ children, toggled, onClick }) {
   const [isToggled, toggle] = useState(toggled);
+
+  // Bugg: Om en toggle har blivit ändrad utifrån så uppdateras den inte
+  // Det fixas genom att klicka en gång. Så i UI perspektiv så gör det inte så mycket.
+  // Användaren behöver bara trycka två gånger. Ex stänger av en typer med notiser
+  // av så krävs två tryck för att få på en typ
 
   const callback = () => {
     onClick(!isToggled);
