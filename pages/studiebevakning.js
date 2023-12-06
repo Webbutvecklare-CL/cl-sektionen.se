d1fd77e92280ed720e8d327a0705933b9e290e0d
CL-Webbunderhallare <webbunderhallare@cl-sektionen.se>
2023-12-06 13:36:46 +0800

    Fixade text wrapping issue på studiebevakning

--- pages/studiebevakning.js
+++ pages/studiebevakning.js
@@ -5,8 +5,8 @@ export default function Studiebevakning({ contents }) {
   return (
     <div id="contentbody">
       <h1 id="page-title">Studiebevakning</h1>
-      <p>
-        På den här sidan kan du läsa om hur du kan påverka din utbildning och studentliv samt vad
+      <p className="headerText">
+        På denna sidan kan du läsa om hur du kan påverka din utbildning och studentliv samt vad
         det finns för hjälp att få.
       </p>
       <MarkdownRender mdData={contents["influence"]} />
