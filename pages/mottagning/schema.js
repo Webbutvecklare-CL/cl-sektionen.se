0db22f95b60d6a360d38594902e3610e8eb5673c
Emil Rapp <webbutvecklare@cl-sektionen.se>
2024-08-06 14:16:15 +0200

    schema för mottagning

--- pages/mottagning/schema.js
+++ pages/mottagning/schema.js
@@ -1,28 +1,59 @@
 import BackButton from "@/components/BackButton";
 import CustomHead from "@/components/CustomHead";
-import CalendarSubscription from "@/components/calendar/CalendarSubscription";
-import CalendarViewer from "@/components/mottagning/CalendarViewer";
+
+import Schema_v1 from "@/media/mottagning/schema_1.webp";
+import Schema_v2 from "@/media/mottagning/schema_2.webp";
+import Schema_v3 from "@/media/mottagning/schema_3.webp";
+import Schema_legend from "@/media/mottagning/schema_legend.webp";
+
+import Image from "next/image";
 
 export default function Schema() {
-	const calendar_id =
-		"c_1351cc6b384ac29b6abd7b38136ebae1b08e383e3cc6299a3aa90303770f46ed@group.calendar.google.com";
 	return (
 		<>
 			<CustomHead
 				metaTitle={"Mottagningsschema | Sektionen för Civilingenjör och Lärare"}
-				description={"Schemat för mottagningen 2023."}
+				description={"Schemat för mottagningen 2024."}
 				url={"https://www.cl-sektionen.se/mottagning/schema"}
 			/>
 			<div id="contentbody" className="wideContent">
 				<BackButton page="mottagning">Mottagningssidan</BackButton>
 				<div>
-					<h1>Mottagning - Schema</h1>
-				</div>
-				<div>
-					<CalendarSubscription calendar_id={calendar_id}>
-						<strong>Lägg till i din egna kalender:</strong>
-					</CalendarSubscription>
-					<CalendarViewer calendar_id={calendar_id} />
+					<article>
+						<h1>Mottagningsschema</h1>
+						<h2>Första veckan, v.33</h2>
+						<Image
+							src={Schema_v1}
+							width={0}
+							height={0}
+							sizes="100vw"
+							style={{ width: "100%", height: "auto" }}
+						/>
+						<h2>Andra veckan, v.34</h2>
+						<Image
+							src={Schema_v2}
+							width={0}
+							height={0}
+							sizes="100vw"
+							style={{ width: "100%", height: "auto" }}
+						/>
+						<h2>Tredje veckan, v.35</h2>
+						<Image
+							src={Schema_v3}
+							width={0}
+							height={0}
+							sizes="100vw"
+							style={{ width: "100%", height: "auto" }}
+						/>
+						<h2>Teckenförklaring</h2>
+						<Image
+							src={Schema_legend}
+							width={0}
+							height={0}
+							sizes="100vw"
+							style={{ width: "100%", height: "auto" }}
+						/>
+					</article>
 				</div>
 			</div>
 		</>
