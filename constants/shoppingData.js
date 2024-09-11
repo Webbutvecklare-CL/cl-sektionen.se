e696c114d7d7798bbe8ce7e4678a65aa8a37cb20
Emil Rapp <webbutvecklare@cl-sektionen.se>
2024-09-11 10:09:17 +0200

    webbshop test-redo

---
+++ constants/shoppingData.js
@@ -0,0 +1,36 @@
+const shoppingData = {
+	items: [
+		{
+			id: "small",
+			name: "Cute small horse",
+			price: "129",
+			description: "Teeny tiny horsy",
+		},
+		{
+			id: "right",
+			name: "Cool horse",
+			price: "12",
+			description: "Wow",
+		},
+		{
+			id: "rome",
+			name: "Roman Horse",
+			price: "13,5",
+			description: "Tänker du ofta på honom?",
+		},
+		{
+			id: "sherlock",
+			name: "Sherlock Horse",
+			price: "200",
+			description: "Elementary my dear Horson",
+		},
+		{
+			id: "magic",
+			name: "Pling plong",
+			price: "1337",
+			description: "Horse with magic powers",
+		},
+	],
+};
+
+export { shoppingData };
