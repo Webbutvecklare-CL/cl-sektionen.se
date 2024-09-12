03298270411bda752e612ed1090397759d4c3e4a
Emil Rapp <webbutvecklare@cl-sektionen.se>
2024-09-12 14:04:14 +0200

    nice buttons

--- pages/cl-shop/index.js
+++ pages/cl-shop/index.js
@@ -1,9 +1,26 @@
 import CustomHead from "@/components/CustomHead";
 import ShopCard from "@/components/ShopCard";
 import { patchData, shoppingData } from "@/constants/shoppingData";
+
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import styles from "@/styles/cl-shop.module.css";
+import { useState } from "react";
 
 export default function CL_shop() {
+	const [isDrawerOpen, setIsDrawerOpen] = useState({
+		shopping: true,
+		patches: true,
+	});
+
+	const toggleDrawer = (section) => {
+		setIsDrawerOpen((prevState) => ({
+			...prevState,
+			[section]: !prevState[section],
+		}));
+	};
+
 	return (
 		<>
 			<CustomHead
@@ -14,24 +31,58 @@ export default function CL_shop() {
 				url={"https://www.cl-sektionen.se/webbshopp"}
 			/>
 			<div id="contentbody">
-				<h1 id={"page-title"}>CL-shop</h1>
+				<h1 id="page-title">CL-shop</h1>
 				<p>
 					För att beställa behöver du kontakta sektionens Försäljningsansvarig.
 					Du betalar genom att swisha sektionen på numret:{" "}
 					<span>123-29 38 108</span>
 				</p>
-				<h2>Sektions-prylar</h2>
-				<ul className={styles.itemList}>
-					{shoppingData.items.map((item) => (
-						<ShopCard key={item.id} item={item} />
-					))}
-				</ul>
-				<h2>Märken</h2>
-				<ul className={styles.itemList}>
-					{patchData.items.map((item) => (
-						<ShopCard key={item.id} item={item} />
-					))}
-				</ul>
+
+				<div className={styles.drawerSection}>
+					<button
+						type="button"
+						onClick={() => toggleDrawer("shopping")}
+						className={styles.drawerToggle}
+					>
+						Sektions-prylar
+						<FontAwesomeIcon
+							icon={isDrawerOpen.shopping ? faAngleUp : faAngleDown}
+							className={styles.drawerIcon}
+						/>
+					</button>
+					<div
+						className={`${styles.drawerContent} ${isDrawerOpen.shopping ? styles.open : styles.closed}`}
+					>
+						<ul className={styles.itemList}>
+							{shoppingData.items.map((item) => (
+								<ShopCard key={item.id} item={item} />
+							))}
+						</ul>
+					</div>
+				</div>
+
+				<div className={styles.drawerSection}>
+					<button
+						type="button"
+						onClick={() => toggleDrawer("patches")}
+						className={styles.drawerToggle}
+					>
+						Märken
+						<FontAwesomeIcon
+							icon={isDrawerOpen.patches ? faAngleUp : faAngleDown}
+							className={styles.drawerIcon}
+						/>
+					</button>
+					<div
+						className={`${styles.drawerContent} ${isDrawerOpen.patches ? styles.open : styles.closed}`}
+					>
+						<ul className={styles.itemList}>
+							{patchData.items.map((item) => (
+								<ShopCard key={item.id} item={item} />
+							))}
+						</ul>
+					</div>
+				</div>
 			</div>
 		</>
 	);
