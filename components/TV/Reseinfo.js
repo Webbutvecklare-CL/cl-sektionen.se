4e3b58c53487032e0f6b66cddd0e22e192cd0963
Emil Rapp <webbutvecklare@cl-sektionen.se>
2024-11-08 12:56:58 +0100

    start of apu

--- components/TV/Reseinfo.js
+++ components/TV/Reseinfo.js
@@ -1,8 +1,18 @@
-import Image from "next/image";
-
 import styles from "@/styles/tv.module.css";
 
-export default function Reseinfo() {
+import TravelGroup from "./TravelGroup";
+
+export async function getServerSideProps() {
+	const res = await fetch(
+		"https://transport.integration.sl.se/v1/sites/9204/departures",
+	);
+	console.log("res", res);
+	const dataSL = await res.json();
+
+	return { props: { dataSL } };
+}
+
+export default function Reseinfo({ dataSL }) {
 	const fakeInfo = [
 		{
 			name: "Tunnelbana",
@@ -45,31 +55,7 @@ export default function Reseinfo() {
 			{fakeInfo.map((item) => (
 				<TravelGroup key={item.name} {...item} />
 			))}
-		</div>
-	);
-}
-
-function TravelGroup({ name, icon, data }) {
-	return (
-		<div className={styles.travelGroup}>
-			<div className={styles.titleGroup}>
-				<Image src={`/media/tv/${icon}.svg`} width={32} height={32} />
-				<h2>{name}</h2>
-			</div>
-			<table className={styles.travelTable}>
-				<tr className={styles.travelTableHeader}>
-					<th className={styles.travelTableLine}>Linje</th>
-					<th className={styles.travelTableDestination}>Mot</th>
-					<th className={styles.travelTableTime}>Tid</th>
-				</tr>
-				{data.map((item, index) => (
-					<tr key={index}>
-						<td>{item.line}</td>
-						<td>{item.destination}</td>
-						<td>{item.time}</td>
-					</tr>
-				))}
-			</table>
+			<p>{dataSL}</p>
 		</div>
 	);
 }
